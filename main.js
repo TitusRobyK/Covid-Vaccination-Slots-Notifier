5664b7cba3f6345c7327d5d04c37f2e681178202
Titus Roby K <titusrobyk@gmail.com>
2021-05-08 17:05:38 +0530

    Initial commit for setting up of Mail Notification..

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const userinput = require("./src/fetchUserInput");
 const vaccineslots = require("./src/checkAvailability");
 const notify = require("./src/notifyUser");
+const cron = require('node-schedule');
 
 async function main() {
-    console.log("Covid Vaccination Notifier - Would periodically check vaccine availability & based on the current availability, desktop notification would be sent.");
+
+    console.log("Covid Vaccination Notifier - Would periodically Check Slot availability & based on the current availability, Notification would be sent.");
     let userInput = await userinput.fetchUserInput();
-    //console.log(" User Input :", JSON.stringify(userInput, null, 2));
-    let response = await vaccineslots.checkVaccineAvailabilityFor(userInput);
-    //console.log(" API Response :", JSON.stringify(response, null, 2));
-    let messageResponse = vaccineslots.createNotificationMessage(response);
-    console.log(messageResponse);
-    if (messageResponse != '') {
-        notify.send(messageResponse);
-    }
+    console.log(" User Input :", JSON.stringify(userInput, null, 2));
+
+    //const cronjob = cron.scheduleJob('*/3 * * * *', async function () {
+        let response = await vaccineslots.checkVaccineAvailabilityFor(userInput);
+        //console.log(" API Response :", JSON.stringify(response, null, 2));
+        let messageResponse = vaccineslots.createNotificationMessage(response);
+        console.log(messageResponse);
+        if (messageResponse != '') {
+            notify.send(messageResponse);
+        }
+    //});
 }
 
 main().then(() => { });
--- src/fetchUserInput.js
+++ src/fetchUserInput.js
@@ -1,9 +1,10 @@
 'use strict';
 const inquirer = require('inquirer');
+const emailValidator = require("email-validator");
 
 exports.fetchUserInput = async function () {
     console.log('Hi, Initializing..');
-    var questions = [
+    let questions = [
         {
             type: 'input',
             name: 'pincode',
@@ -13,25 +14,83 @@ exports.fetchUserInput = async function () {
                 if (pass) {
                     return true;
                 }
-
                 return 'Please enter a valid pincode 🧐';
-            },
+            }
         },
         {
             type: 'input',
             name: 'age',
             message: "What's your age ?",
             validate: function (value) {
-                var pass = value.match("^[1-9]{1,2}$");
+                var pass = value.match("^[0-9]{1,2}$");
                 if (pass) {
                     return true;
                 }
-
                 return 'Please enter a valid age 🧐';
-            },
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'isEmailNotificationRequired',
+            message: "Do you require Email Notification ? ",
         }
     ];
 
+    let answers = await inquirer.prompt(questions);
+    if (answers.isEmailNotificationRequired) {
+        answers["emailDetails"] = await fetchEmailSetupDetails();
+    }
+    console.log(' Thank you for your response , The App will run in background..');
+    return answers;
+}
+
+async function fetchEmailSetupDetails() {
+    let questions = [
+        {
+            type: 'list',
+            name: 'mailservice',
+            message: "Input the Mail Service ",
+            choices: ['Gmail'],
+            filter: function (val) {
+                return val.toLowerCase();
+            },
+        },
+        {
+            type: 'input',
+            name: 'smtpHost',
+            message: "Enter the SMTP Host ?",
+        },
+        {
+            type: 'input',
+            name: 'authUserName',
+            message: "Input Email Service Auth User ",
+            validate: function (value) {
+                var pass = emailValidator.validate(value);
+                if (pass) {
+                    return true;
+                }
+                return 'Please input a valid Email Service Auth User  🧐🧐';
+            },
+        },
+        {
+            type: 'password',
+            name: 'authPassword',
+            message: "Input Email Service Auth Password ",
+            mask: '*'
+        },
+        {
+            type: 'input',
+            name: 'recieverMailId',
+            message: "Input Email to which Vaccine Notification Needs to be sent..",
+            validate: function (value) {
+                var pass = emailValidator.validate(value);
+                if (pass) {
+                    return true;
+                }
+                return 'Please input a valid Email Id 🧐🧐';
+            }
+        }
+    ];
     let answers = await inquirer.prompt(questions);
     return answers;
 }
