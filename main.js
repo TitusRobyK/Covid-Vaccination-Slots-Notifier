3c8c4b4a5987c5d66f795577df51b640d348dc88
Titus Roby K <titusrobyk@gmail.com>
2021-06-20 00:01:07 +0530

    Adding a internet connection check ..

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 const userinput = require("./lib/fetchUserInput");
 const vaccineslots = require("./lib/checkAvailability");
 const notify = require("./lib/notifyUser");
-const cron = require('node-schedule');
-const paintTerminal = require('./lib/paintTerminal');
+const cron = require("node-schedule");
+const paintTerminal = require("./lib/paintTerminal");
+const chalk = require("chalk");
 
 async function main() {
-    paintTerminal();
-    let userInput = await userinput.fetchUserInput();
-    const cronjob = cron.scheduleJob('*/3 * * * *', async function () {
-        let response = await vaccineslots.checkVaccineAvailabilityFor(userInput);
-        let notificationMessage = vaccineslots.createNotificationMessage(response);
-        if (notificationMessage != '') {
-            notify.send(notificationMessage);
-            if (userInput.isEmailNotificationRequired) {
-                await notify.sendEmail(userInput.emailDetails, notificationMessage);
-            }
-        }
-    });
-}
+  paintTerminal();
+  let userInput = await userinput.fetchUserInput();
+  const cronjob = cron.scheduleJob("*/3 * * * *", async function () {
 
-main().then(() => { });
+    let isConnected = !!(await require("dns")
+      .promises.resolve("google.com")
+      .catch(() => {}));
+
+    if (isConnected) {
+      let response = await vaccineslots.checkVaccineAvailabilityFor(userInput);
+      let notificationMessage = vaccineslots.createNotificationMessage(response);
+      if (notificationMessage != "") {
+        notify.send(notificationMessage);
+        if (userInput.isEmailNotificationRequired) {
+          await notify.sendEmail(userInput.emailDetails, notificationMessage);
+        }
+      }
+    } else {
+      console.log(chalk.red("\nMake sure you're Online. Reconnecting after 3 minutes."));
+    }
+  });
+}
+main().then(() => {});
