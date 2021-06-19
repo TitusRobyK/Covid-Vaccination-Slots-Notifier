aa07e343e059b9f1bfc154b9e86d759e2dd8a10c
Titus Roby K <titusrobyk@gmail.com>
2021-06-19 20:28:41 +0530

    Enhancing the interaction..

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const userinput = require("./lib/fetchUserInput");
 const vaccineslots = require("./lib/checkAvailability");
 const notify = require("./lib/notifyUser");
 const cron = require('node-schedule');
+const paintTerminal = require('./lib/paintTerminal');
 
 async function main() {
-
-    console.log("Covid Vaccine Slots Notifier - Would periodically Check Slot availability & based on the current availability,Notification would be sent\n");
+    paintTerminal();
     let userInput = await userinput.fetchUserInput();
     const cronjob = cron.scheduleJob('*/3 * * * *', async function () {
         let response = await vaccineslots.checkVaccineAvailabilityFor(userInput);
